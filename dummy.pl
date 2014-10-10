c5d90332e45a34711a01d739468b7526a1e5107b
amit-mittal <amit.mittal1993@gmail.com>
2014-10-11 03:17:04 +0530

    Conditional equal sign working

--- dummy.pl
+++ dummy.pl
@@ -1,6 +1,35 @@
 /*Expression Evaluation*/
-eval(A * B, CV, Vars):- eval_v(A, AV, Vars), eval_v(B, BV, Vars), CV is AV * BV.
-eval(A + B, CV, Vars):- eval_v(A, AV, Vars), eval_v(B, BV, Vars), CV is AV + BV.
-eval(A - B, CV, Vars):- eval_v(A, AV, Vars), eval_v(B, BV, Vars), CV is AV - BV.
-eval(Num, Num, Vars):- number(Num).
-eval(Var, Value, Vars):- atom(Var), member(Var/Value, Vars).
+eval_v(A * B, CV, Vars):- eval_v(A, AV, Vars), eval_v(B, BV, Vars), CV is AV * BV.
+eval_v(A + B, CV, Vars):- eval_v(A, AV, Vars), eval_v(B, BV, Vars), CV is AV + BV.
+eval_v(A - B, CV, Vars):- eval_v(A, AV, Vars), eval_v(B, BV, Vars), CV is AV - BV.
+eval_v(Num, Num, Vars):- number(Num).
+eval_v(Var, Value, Vars):- atom(Var), member(Var/Value, Vars).
+
+/*Boolean Constants*/
+logic_const(true).
+logic_const(false).
+
+/*Boolean Definitions*/
+and_d(false,true,false).
+and_d(false,false,false).
+and_d(true,false,false).
+and_d(true,true,true).
+   
+or_d(false,true,true).
+or_d(false,false,false).
+or_d(true,false,true).
+or_d(true,true,true).
+   
+non_d(true,false).
+non_d(false,true).
+
+/*Boolean Expression Evaluation*/
+eval_b(X, X):- logic_const(X).
+/*
+eval_b(X and Y, R):- eval_b(X,XV), eval_b(Y,YV), and_d(XV,YV,R).
+eval_b(X or Y, R):- eval_b(X,XV),eval_b(Y,YV),or_d(XV,YV,R).
+eval_b(non X, R):- eval_b(X,XV),non_d(XV,R).
+*/
+
+eval_b(A == B, R):- eval_v(A, CV1, _), eval_v(B, CV2, _), CV1 == CV2, R = true.
+eval_b(A == B, R):- eval_v(A, CV1, _), eval_v(B, CV2, _), CV1 =\= CV2, R = false.
