487b62b4024e878b1b417fc702d6037e38457b95
amit-mittal <amit.mittal1993@gmail.com>
2014-10-12 01:11:04 +0530

    find_replace_insert implemented

--- dummy.pl
+++ dummy.pl
@@ -1,3 +1,17 @@
+/*Helper Functions*/
+/*
+append([], L, L). 
+append([H|T], L2, [H|L3]):-  append(T, L2, L3).
+*/
+
+/*Find and Replace or Insert*/
+find_replace_insert(Var, Value, [], [Var/Value]).
+find_replace_insert(Var, Value, [Var/Oldval|Vars], [Var/Value|Vars]).
+find_replace_insert(Var, Value, [DVar/DValue|Vars], [DVar/DValue|NVars]):- find_replace_insert(Var, Value, Vars, NVars).
+
+/*Assignment Evaluation*/
+/*eval_a(assign(X, Y), Vars):- */
+
 /*Expression Evaluation*/
 eval_v(A * B, CV, Vars):- eval_v(A, AV, Vars), eval_v(B, BV, Vars), CV is AV * BV.
 eval_v(A + B, CV, Vars):- eval_v(A, AV, Vars), eval_v(B, BV, Vars), CV is AV + BV.
@@ -14,22 +28,23 @@ and_d(false,true,false).
 and_d(false,false,false).
 and_d(true,false,false).
 and_d(true,true,true).
-   
+
 or_d(false,true,true).
 or_d(false,false,false).
 or_d(true,false,true).
 or_d(true,true,true).
-   
+
 non_d(true,false).
 non_d(false,true).
 
 /*Boolean Expression Evaluation*/
 eval_b(X, X):- logic_const(X).
 /*
-eval_b(X and Y, R):- eval_b(X,XV), eval_b(Y,YV), and_d(XV,YV,R).
-eval_b(X or Y, R):- eval_b(X,XV),eval_b(Y,YV),or_d(XV,YV,R).
-eval_b(non X, R):- eval_b(X,XV),non_d(XV,R).
+eval_b(X and Y, R):- eval_b(X, XV), eval_b(Y, YV), and_d(XV, YV, R).
+eval_b(X or Y, R):- eval_b(X, XV), eval_b(Y, YV),or_d(XV, YV, R).
+eval_b(non X, R):- eval_b(X, XV), non_d(XV, R).
 */
 
+/*Conditional Expression Evaluation*/
 eval_b(A == B, R):- eval_v(A, CV1, _), eval_v(B, CV2, _), CV1 == CV2, R = true.
 eval_b(A == B, R):- eval_v(A, CV1, _), eval_v(B, CV2, _), CV1 =\= CV2, R = false.
