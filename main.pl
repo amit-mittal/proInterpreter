5d0fc4baf6a453e68c14b4e8a5cc9a3cd781ab77
amit-mittal <amit.mittal1993@gmail.com>
2014-10-14 00:05:47 +0530

    warnings fixed

--- main.pl
+++ main.pl
@@ -1,6 +1,6 @@
 /*Find and Replace or Insert*/
 find_replace_insert(Var, Value, [], [Var/Value]).
-find_replace_insert(Var, Value, [Var/Oldval|Vars], [Var/Value|Vars]):- !.
+find_replace_insert(Var, Value, [Var/_|Vars], [Var/Value|Vars]):- !.
 find_replace_insert(Var, Value, [DVar/DValue|Vars], [DVar/DValue|NVars]):- find_replace_insert(Var, Value, Vars, NVars).
 
 /*Boolean Constants*/
@@ -21,18 +21,18 @@ or_d(true, true, true).
 non_d(true, false).
 non_d(false, true).
 
-/*Expression Evaluation*/
-eval(A * B, CV, Vars):- eval(A, AV, Vars), eval(B, BV, Vars), CV is AV * BV.
-eval(A + B, CV, Vars):- eval(A, AV, Vars), eval(B, BV, Vars), CV is AV + BV.
-eval(A - B, CV, Vars):- eval(A, AV, Vars), eval(B, BV, Vars), CV is AV - BV.
-eval(Num, Num, Vars):- number(Num).
-eval(Var, Value, Vars):- atom(Var), member(Var/Value, Vars).
-
 /*Boolean Expression Evaluation*/
 eval(X, X):- logic_const(X).
 eval(and(X, Y), R):- eval(X, XV), eval(Y, YV), and_d(XV, YV, R).
 eval(or(X, Y), R):- eval(X, XV), eval(Y, YV), or_d(XV, YV, R).
 eval(not(X), R):- eval(X, XV), non_d(XV, R).
+
+/*Expression Evaluation*/
+eval(A * B, CV, Vars):- eval(A, AV, Vars), eval(B, BV, Vars), CV is AV * BV.
+eval(A + B, CV, Vars):- eval(A, AV, Vars), eval(B, BV, Vars), CV is AV + BV.
+eval(A - B, CV, Vars):- eval(A, AV, Vars), eval(B, BV, Vars), CV is AV - BV.
+eval(Num, Num, _):- number(Num).
+eval(Var, Value, Vars):- atom(Var), member(Var/Value, Vars).
 
 /*Conditional Expression Evaluation*/
 eval(A == B, true, Vars):- eval(A, CV1, Vars), eval(B, CV2, Vars), CV1 == CV2.
@@ -50,13 +50,13 @@ eval(A =< B, false, Vars):- eval(A, CV1, Vars), eval(B, CV2, Vars), CV1 > CV2.
 eval(assign(X, Y), OVars, NVars):- eval(Y, Val, OVars), find_replace_insert(X, Val, OVars, NVars).
 
 /*If-else Evaluation*/
-eval(ifthenelse(Expr, compound(X), compound(Y)), OVars, NVars):- eval(Expr, Boolval, OVars), 
+eval(ifthenelse(Expr, compound(X), compound(_)), OVars, NVars):- eval(Expr, Boolval, OVars), 
 		Boolval == true, eval(compound(X), OVars, NVars), !.
-eval(ifthenelse(Expr, compound(X), compound(Y)), OVars, NVars):- eval(Expr, Boolval, OVars), 
+eval(ifthenelse(Expr, compound(_), compound(Y)), OVars, NVars):- eval(Expr, Boolval, OVars), 
 		Boolval == false, eval(compound(Y), OVars, NVars).
 
 /*While Evaluation*/
-eval(while(A, compound(X)), OVars, OVars):- eval(A, Boolval, OVars), Boolval == false, !.
+eval(while(A, compound(_)), OVars, OVars):- eval(A, Boolval, OVars), Boolval == false, !.
 eval(while(A, compound(X)), OVars, NVars):- eval(A, Boolval, OVars), Boolval == true, eval(compound(X), OVars, IVars), 
 		eval(while(A, compound(X)), IVars, NVars).
 
@@ -70,4 +70,3 @@ eval(compound([A|B]), OVars, NVars):- eval(A, OVars, IVars), eval(compound(B), I
 
 /*TODO: if possible support brackets*/
 /*TODO: if possible support single if statement whithout else*/
-/*TODO: remove warnings*/
